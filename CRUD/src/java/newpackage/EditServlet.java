b63ec3169b8bd9549ce5d3397be7ac0c49df16d7
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-28 22:48:05 +0530

    Create EditServlet.java

---
+++ CRUD/src/java/newpackage/EditServlet.java
@@ -0,0 +1,58 @@
+
+package newpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author samee
+ */
+@WebServlet(name = "EditServlet", urlPatterns = {"/EditServlet"})
+public class EditServlet extends HttpServlet {
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       PrintWriter out= response.getWriter();
+       response.setContentType("text/html");
+          out.println("<h1>Update Employee</h1>");  
+        String sid=request.getParameter("id");  
+        int id=Integer.parseInt(sid);  
+          
+        Emp e=EmpDao.getEmployeeById(id);  
+          
+        out.print("<form action='EditServlet2'>");  
+        out.print("<table>");  
+        out.print("<tr><td></td><td><input type='hidden' name='id' value='"+e.getId()+"'/></td></tr>");  
+        out.print("<tr><td>Name:</td><td><input type='text' name='name' value='"+e.getName()+"'/></td></tr>");  
+        out.print("<tr><td>Password:</td><td><input type='text' name='password' value='"+e.getPassword()+"'/>  </td></tr>");  
+        out.print("<tr><td>Email:</td><td><input type='email' name='email' value='"+e.getEmail()+"'/></td></tr>");  
+        out.print("<tr><td>Country:</td><td>");  
+        out.print("<select name='country' style='width:150px'>");  
+        out.print("<option>India</option>");  
+        out.print("<option>USA</option>");  
+        out.print("<option>UK</option>");  
+        out.print("<option>Other</option>");  
+        out.print("</select>");  
+        out.print("</td></tr>"); 
+        
+        out.print("<tr><td colspan='2'><input type='submit' value='Edit & Save '/></td></tr>");  
+        out.print("</table>");  
+        out.print("</form>");  
+          
+        out.close();  
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      
+    }
+}
